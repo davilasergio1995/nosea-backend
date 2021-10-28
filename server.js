4a1c7eaea5aaae7858d82d7bdb75fe2757b4a803
Sergio <snowhopfirado@gmail.com>
2021-10-28 17:26:44 -0400

    Added post request support (only halfway, yet to write to .JSON file)

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const alessa = express();
 const path = require('path');
 const chatJson = path.join(__dirname, '/database', 'chat.json');
 
+//parses JSON data
+alessa.use(express.json());
+
+//Sends .JSON file 
 alessa.get('/api/chatLogs',(req,res) => {
     res.header("Content-type" , "application/json");
     res.sendFile(chatJson);
 })
+
+//Accepts .JSON data
+alessa.post('/api/chatLogs',(req,res) => {
+    req.body;
+    res.json(req.body);
+    console.log(req.body);
+});
 
 alessa.listen(8000,() => {
     console.log('Backend server listening on port 8000');
