bb96af83b8aa3aa7aeb0ab1619642f46579a2647
Sergio <snowhopfirado@gmail.com>
2021-10-28 21:23:45 -0400

    Figured out how to push data into .JSON file. Functioning as expected.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const alessa = express();
 const path = require('path');
 const chatJson = path.join(__dirname, '/database', 'chat.json');
+const {readFile, writeFile} = require('fs');
 
 //parses JSON data
 alessa.use(express.json());
@@ -15,8 +16,20 @@ alessa.get('/api/chatLogs',(req,res) => {
 //Accepts .JSON data
 alessa.post('/api/chatLogs',(req,res) => {
     req.body;
-    res.json(req.body);
-    console.log(req.body);
+    let test = JSON.stringify(req.body);
+    readFile(chatJson,(err,data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let json = JSON.parse(data);
+            json.push(req.body);
+            writeFile(chatJson,JSON.stringify(json), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+    });
 });
 
 alessa.listen(8000,() => {
