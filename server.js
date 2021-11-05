0a61edb01079b726bb6f8c54d8d1e40ba9309242
Sergio <snowhopfirado@gmail.com>
2021-11-04 21:18:01 -0400

    Created API to handle login requests. Sends back a simple string letting user know the result of their request. Not secure yet, very vulnerable.

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ const express = require('express');
 const alessa = express();
 const path = require('path');
 const chatJson = path.join(__dirname, '/database', 'chat.json');
+const users = path.join(__dirname, '/database','users.json');
 const {readFile, writeFile} = require('fs');
 
 //parses JSON data
 alessa.use(express.json());
 
+alessa.use(express.static('database'));
+
 //Sends .JSON file 
 alessa.get('/api/chatLogs',(req,res) => {
+
     res.header("Content-type" , "application/json");
     res.sendFile(chatJson);
-})
+    
+});
 
-//Accepts .JSON data
-alessa.post('/api/chatLogs',(req,res) => {
+
+//Accepts .JSON data and pushes into .JSON
+alessa.post('/api/chatLogs/',(req,res) => {
+    res.sendStatus(200);
     req.body;
-    let test = JSON.stringify(req.body);
     readFile(chatJson,(err,data) => {
         if (err) {
             console.log(err);
@@ -31,6 +37,24 @@ alessa.post('/api/chatLogs',(req,res) => {
         }
     });
 });
+
+alessa.post('/api/users', (req,res) => {
+    const login = req.body;
+    readFile(users,(err,data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let json = JSON.parse(data);
+            json.forEach((e) => {
+                if (e.username === login.username && e.password === login.password) {
+                    res.send('yes');
+                } else {
+                    res.send('no')
+                }
+            });
+        }
+    })
+})
 
 alessa.listen(8000,() => {
     console.log('Backend server listening on port 8000');
