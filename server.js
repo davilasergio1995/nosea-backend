eeb695a72b1d5159d78136bd1af723f8277a3f7c
Sergio <snowhopfirado@gmail.com>
2021-11-08 22:31:07 -0500

    Replaced old login/signup functionality. Signup much more secure (hashed passswords), successful login returns a temporary token.

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const path = require('path');
 const chatJson = path.join(__dirname, '/database', 'chat.json');
 const users = path.join(__dirname, '/database','users.json');
 const {readFile, writeFile} = require('fs');
+const bcrypt = require('bcrypt');
+const util = require('util');
+const e = require('express');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 //parses JSON data
 alessa.use(express.json());
@@ -15,7 +20,7 @@ alessa.use(express.json());
 alessa.use(express.static('database'));
 
 //Sends chat log .JSON file [1]
-alessa.get('/api/chatLogs',(req,res) => {
+alessa.get('/api/chatLogs/',(req,res) => {
 
     res.header("Content-type" , "application/json");
     res.sendFile(chatJson);
@@ -42,24 +47,59 @@ alessa.post('/api/chatLogs/',(req,res) => {
     });
 });
 
-//Receives login info from front end, then compares it to the data inside the users.json file [2]
-alessa.post('/api/users', (req,res) => {
-    const login = req.body;
-    readFile(users,(err,data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            let json = JSON.parse(data);
-            json.forEach((e) => {
-                if (e.username === login.username && e.password === login.password) {
-                    res.send('yes');
+//Takes in user signup info and compares it to DB. if user already exists, throws back error. If user does not exists, creates a new
+//entry into the database with a hashed password.
+alessa.post('/api/users/signup', async(req,res) => {
+    try {
+        const user = req.body;
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        const newUser = {firstName: user.firstName, lastName: user.lastName, username: user.username, password: hashedPassword, email: user.email};
+        const fileRead = await readFilePromise(users);
+        const parsedUsers = JSON.parse(fileRead)
+        if (parsedUsers.length === 0) {
+            parsedUsers.push(newUser);
+            await writeFilePromise(users, JSON.stringify(parsedUsers));
+            res.send('ok');
+        };
+        if (parsedUsers.length !== 0) {
+            parsedUsers.forEach((e) => {
+                if (e.username === user.username) {
+                    res.send('Username already exists');
+                } else if (e.email === user.email) {
+                    res.send('Email already registered');
                 } else {
-                    res.send('no')
+                    parsedUsers.push(newUser);
+                    writeFilePromise(users, JSON.stringify(parsedUsers));
+                    res.send('ok');
                 }
-            });
+            })
+        };   
+    } catch {
+        res.status(401);
+    }
+});
+
+//Signs user in. If user exists and username/password are valid, server returns a token. If user doesn't exist or username/password
+//aren't valid, user receives an error.
+alessa.post('/api/users/login', async (req,res) => {
+    let login=req.body;
+    let fileRead = await readFilePromise(users);
+    let parsedUsers = JSON.parse(fileRead);
+    let user = parsedUsers.find(e => e.username === login.username);
+    if (user === null) {
+        res.status(400).send('User doesn\'t exist');
+    };
+    try {
+        if (await bcrypt.compare(login.password,user.password)) {
+            res.send('a-ok');
+        } else {
+            res.send('Username/password incorrect');
         }
-    })
-})
+    } catch {
+        res.status(500).send();
+    }
+});
+
 
 //Activates server, logs a message when successful
 alessa.listen(8000,() => {
